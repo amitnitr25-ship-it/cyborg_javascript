77a0bf50caea4686b3d04e466034ffcce0827ff3
amitnitr25-ship-it <amitnitr25@gmail.com>
2026-05-30 10:37:37 +0530

    Initial commit

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 1234567890;
+let accountName = "John Doe";
+var accountBalance = 1000.50;
+city="New York";
+console.log("Account ID:", accountId);
+console.table({
+    "Account Name": accountName,
+    "Account Balance": accountBalance,
+    "City": city
+});
---
+++ 02_datatype.js
@@ -0,0 +1,8 @@
+"use strict";
+// alert("Welcome to JavaScript!"); it will work here 
+console.log(typeof "Hello, World!"); // Output: string
+console.log(typeof 42); // Output: number
+console.log(typeof true); // Output: boolean
+console.log(typeof undefined); // Output: undefined
+console.log(typeof null); // Output: object (this is a quirk in JavaScript)
+console.log(typeof Symbol("symbol")); // Output: symbol
