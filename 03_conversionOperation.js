3fe8613a818c89869080299464e634ffb7e54a02
amitnitr25-ship-it <amitnitr25@gmail.com>
2026-05-31 20:08:17 +0530

    string and num adding property

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -11,8 +11,16 @@ console.log(scoreInnumber)  //NaN
 //so it hasn't stored the value as a number rather just converted it into number
 // console.log(33ab==scoreInnumber );
 
-let score = null;
-console.log(typeof(score)); //null
-let scoreInnumber= Number(score)
-console.log(typeof(scoreInnumber)) //number
-console.log(scoreInnumber)  //NaN
+let test = null;
+console.log(typeof(test)); //null
+let testInNumber= Number(test)
+console.log(typeof(testInNumber)) //number
+console.log(testInNumber)  //0
+//for true it will show 1
+let isloggedIn=""//for no inputi string the bool is false
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn); //false
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(3+3+"4") //64           
+console.log("1"+3+3)//133
+// String + Anything or Anything + String will always force the entire operation to become a string concatenation
